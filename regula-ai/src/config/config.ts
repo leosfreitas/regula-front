9b452ef4d4ae2cb11d3b48878fffa9da4c01e77e
Thiago Penha <thiagoppc@al.insper.edu.br>
2025-05-26 19:28:14 -0300

    config

--- regula-ai/src/config/config.ts
+++ regula-ai/src/config/config.ts
@@ -1,3 +1,5 @@
 export const config = { 
-    apiBaseUrl: window.location.origin == 'http://localhost:5173' ? "https://regula-back-805c0d9fbbe1.herokuapp.com/" : "URL DEPLOY"
+  apiBaseUrl: window.location.origin === 'http://localhost:5173'
+    ? "http://localhost:8000"
+    : "https://regula-back-805c0d9fbbe1.herokuapp.com/"
 }
