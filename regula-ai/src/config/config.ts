322836ed839a910cc40f2493db31e4437c1112a3
Thiago Penha <thiagoppc@al.insper.edu.br>
2025-05-26 19:30:40 -0300

    config

--- regula-ai/src/config/config.ts
+++ regula-ai/src/config/config.ts
@@ -1,5 +1,5 @@
 export const config = { 
   apiBaseUrl: window.location.origin === 'http://localhost:5173'
     ? "http://localhost:8000"
-    : "https://regula-back-805c0d9fbbe1.herokuapp.com/"
+    : "https://regula-back-805c0d9fbbe1.herokuapp.com"
 }
